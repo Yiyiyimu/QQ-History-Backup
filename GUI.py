0726e00c77d98aabe2d48c0516e6e0620027a19d
Yiyiyimu <wosoyoung@gmail.com>
2020-05-22 21:52:42 +0800

    yapf formatter

--- GUI.py
+++ GUI.py
@@ -7,32 +7,37 @@ import QQ_History
 import os
 import webbrowser
 
+
 def Enter():
-    db, qq, key, msg, n1, n2 = e1.get(), e2.get(), e3.get(), e4.get(), e5.get(), e6.get()
-    if(db == "" or qq == "" or (key == "" and msg == "")):
+    db, qq, key, msg, n1, n2 = e1.get(), e2.get(), e3.get(), e4.get(), e5.get(
+    ), e6.get()
+    if (db == "" or qq == "" or (key == "" and msg == "")):
         info.set("信息不完整！")
-        return()
+        return ()
     info.set("开始导出")
     try:
         realkey = QQ_History.main(db, qq, key, msg, n1, n2)
     except Exception as e:
         info.set(repr(e))
-        return()        
-    if(key == ""):
+        return ()
+    if (key == ""):
         keyGet.set(realkey)
     info.set("完成")
+
 
 def SelectPath():
     pathTmp = filedialog.askopenfilename()
     pathGet.set(pathTmp)
 
+
 def url():
     webbrowser.open_new("https://github.com/Yiyiyimu/QQ_History_Backup")
+
 
 root = tk.Tk()
 pathGet, keyGet, info = tk.StringVar(), tk.StringVar(), tk.StringVar()
 
-tmp = open("tmp.ico","wb+")
+tmp = open("tmp.ico", "wb+")
 tmp.write(base64.b64decode(ico))
 tmp.close()
 root.iconbitmap("tmp.ico")
@@ -40,44 +45,44 @@ os.remove("tmp.ico")
 
 root.title("QQ聊天记录导出")
 
-ttk.Label(root, text = "*db文件地址：").grid(row = 0, column = 0, sticky = "e")
-e1 = ttk.Entry(root, textvariable = pathGet)
-e1.grid(row = 0, column = 1)
-ttk.Button(root, text = "选择", command = SelectPath, width = 5).grid(row = 0, column = 3)
+ttk.Label(root, text="*db文件地址：").grid(row=0, column=0, sticky="e")
+e1 = ttk.Entry(root, textvariable=pathGet)
+e1.grid(row=0, column=1)
+ttk.Button(root, text="选择", command=SelectPath, width=5).grid(row=0, column=3)
 
-ttk.Label(root, text = "*对方QQ号：").grid(row = 1, column = 0, sticky = "e")
+ttk.Label(root, text="*对方QQ号：").grid(row=1, column=0, sticky="e")
 e2 = ttk.Entry(root)
-e2.grid(row = 1, column = 1, columnspan = 3, sticky = "ew", pady = 3)
+e2.grid(row=1, column=1, columnspan=3, sticky="ew", pady=3)
 
-ttk.Label(root, text = "手机识别码：").grid(row = 2, column = 0, sticky = "e")
-e3 = ttk.Entry(root, textvariable = keyGet)
-e3.grid(row = 2, column = 1, columnspan = 3, sticky = "ew", pady = 3)
+ttk.Label(root, text="手机识别码：").grid(row=2, column=0, sticky="e")
+e3 = ttk.Entry(root, textvariable=keyGet)
+e3.grid(row=2, column=1, columnspan=3, sticky="ew", pady=3)
 
-ttk.Label(root, text = "最后一次聊天记录\n（至少六个汉字）").grid(row = 3, column = 0, sticky = "e")
+ttk.Label(root, text="最后一次聊天记录\n（至少六个汉字）").grid(row=3, column=0, sticky="e")
 e4 = ttk.Entry(root)
-e4.grid(row = 3, column = 1, columnspan = 3, sticky = "ew", pady = 3)
+e4.grid(row=3, column=1, columnspan=3, sticky="ew", pady=3)
 
-ttk.Label(root, text = "我的名字：").grid(row = 4, column = 0, sticky = "e")
+ttk.Label(root, text="我的名字：").grid(row=4, column=0, sticky="e")
 e5 = ttk.Entry(root)
-e5.grid(row = 4, column = 1, columnspan = 3, sticky = "ew", pady = 3)
-ttk.Label(root, text = "对方名字：").grid(row = 5, column = 0, sticky = "e")
+e5.grid(row=4, column=1, columnspan=3, sticky="ew", pady=3)
+ttk.Label(root, text="对方名字：").grid(row=5, column=0, sticky="e")
 e6 = ttk.Entry(root)
-e6.grid(row = 5, column = 1, columnspan = 3, sticky = "ew", pady = 3)
+e6.grid(row=5, column=1, columnspan=3, sticky="ew", pady=3)
 
 root.grid_columnconfigure(2, weight=1)
 
-ttk.Button(root, text = "确认", command = Enter).grid(row = 6, column = 1)
-l1 = ttk.Label(root, textvariable = info)
-l1.grid(row = 7, column = 1)
+ttk.Button(root, text="确认", command=Enter).grid(row=6, column=1)
+l1 = ttk.Label(root, textvariable=info)
+l1.grid(row=7, column=1)
 
-tmp = open("tmp.png","wb+")
+tmp = open("tmp.png", "wb+")
 tmp.write(base64.b64decode(github_mark))
 tmp.close()
-github = tk.PhotoImage(file = 'tmp.png')
+github = tk.PhotoImage(file='tmp.png')
 os.remove("tmp.png")
 
-button_img = tk.Button(root, image = github, text = 'b', command = url, bd = 0)
-button_img.grid(row = 6, rowspan = 7, column = 0,sticky = "ws")
+button_img = tk.Button(root, image=github, text='b', command=url, bd=0)
+button_img.grid(row=6, rowspan=7, column=0, sticky="ws")
 
 root.mainloop()
 
--- QQ_History.py
+++ QQ_History.py
@@ -4,34 +4,36 @@ import sqlite3
 import time
 import traceback
 
+
 class QQoutput():
-    def __init__(self,db,key,mode,s):
-        self.key=key    #解密用的密钥
-        self.c=sqlite3.connect(db).cursor()
-        self.mode=mode
-        self.s=s
-        
-    def fix(self,data,mode):
+    def __init__(self, db, key, mode, s):
+        self.key = key  #解密用的密钥
+        self.c = sqlite3.connect(db).cursor()
+        self.mode = mode
+        self.s = s
+
+    def fix(self, data, mode):
         #msgdata mode=0
         #other mode=1
-        if(mode==0):
-            rowbyte=[]
-            for i in range(0,len(data)):
-                rowbyte.append(data[i]^ord(self.key[i%len(self.key)]))
-            rowbyte=bytes(rowbyte)
+        if (mode == 0):
+            rowbyte = []
+            for i in range(0, len(data)):
+                rowbyte.append(data[i] ^ ord(self.key[i % len(self.key)]))
+            rowbyte = bytes(rowbyte)
             try:
-                msg=rowbyte.decode(encoding="utf-8")
+                msg = rowbyte.decode(encoding="utf-8")
             except:
-                msg=NULL
+                msg = NULL
             return msg
-        elif(mode==1):
-            str=""
+        elif (mode == 1):
+            str = ""
             try:
-                for i in range(0,len(data)):
-                    str+=chr(ord(data[i])^ord(self.key[i%len(self.key)]))
+                for i in range(0, len(data)):
+                    str += chr(ord(data[i]) ^ ord(self.key[i % len(self.key)]))
             except:
-                str=NULL
+                str = NULL
             return str
+
     def decode(self, cursor):
         for row in cursor:
             continue
@@ -39,8 +41,8 @@ class QQoutput():
         MsgEnc = self.s.encode(encoding="utf-8")
         KeySet = ""
         #for i in range(0,min(len(MsgEnc), len(data))):
-        for i in range(0,len(MsgEnc)):
-            KeySet+=chr(data[i]^MsgEnc[i])
+        for i in range(0, len(MsgEnc)):
+            KeySet += chr(data[i] ^ MsgEnc[i])
         #TO AVOID LOOP
         RealKey, restKey = "", ""
         for i in range(4, len(KeySet)):
@@ -50,35 +52,43 @@ class QQoutput():
             To Prevent string loop in single key
             Like "121212456"
             '''
-            RealKey, nextKey, restKey = KeySet[0:i], KeySet[i:2*i], KeySet[2*i:len(KeySet)]
+            RealKey, nextKey, restKey = KeySet[0:i], KeySet[i:2 * i], KeySet[
+                2 * i:len(KeySet)]
             KeyLen = len(RealKey)
             flagLoop = True
             for j in range(KeyLen):
-                if((j < len(nextKey) and RealKey[j] != nextKey[j]) or (j < len(restKey) and RealKey[j] != restKey[j])):
+                if ((j < len(nextKey) and RealKey[j] != nextKey[j])
+                        or (j < len(restKey) and RealKey[j] != restKey[j])):
                     flagLoop = False
                     break
-            if(flagLoop and j == KeyLen-1):
+            if (flagLoop and j == KeyLen - 1):
                 break
         return RealKey
+
     def AddEmoji(self, msg):
         pos = msg.find('\x14')
-        while(pos != -1):
+        while (pos != -1):
             lastpos = pos
-            num = ord(msg[pos+1])
-            msg = msg.replace(msg[pos:pos+2], "<img src='./gif/"+str(num)+".gif' alt="+str(num)+">")
+            num = ord(msg[pos + 1])
+            msg = msg.replace(
+                msg[pos:pos + 2],
+                "<img src='./gif/" + str(num) + ".gif' alt=" + str(num) + ">")
             pos = msg.find('\x14')
-            if(pos == lastpos):
+            if (pos == lastpos):
                 break
         return msg
-    def message(self,num,mode):
+
+    def message(self, num, mode):
         #mode=1 friend
         #mode=2 troop
-        num=str(num).encode("utf-8")
-        md5num=hashlib.md5(num).hexdigest().upper()
-        if(mode==1):
-            execute="select msgData,senderuin,time from mr_friend_{md5num}_New".format(md5num=md5num)
-        elif(mode==2):
-            execute="select msgData,senderuin,time from mr_troop_{md5num}_New".format(md5num=md5num)
+        num = str(num).encode("utf-8")
+        md5num = hashlib.md5(num).hexdigest().upper()
+        if (mode == 1):
+            execute = "select msgData,senderuin,time from mr_friend_{md5num}_New".format(
+                md5num=md5num)
+        elif (mode == 2):
+            execute = "select msgData,senderuin,time from mr_troop_{md5num}_New".format(
+                md5num=md5num)
         else:
             print("error mode")
             exit(1)
@@ -86,39 +96,42 @@ class QQoutput():
         cursor = self.c.execute(execute)
         #except:
         #    raise ValueError("QQ号/db地址错误")
-        if(self.key == "" and len(self.s)>=5):
+        if (self.key == "" and len(self.s) >= 5):
             self.key = self.decode(cursor)
         cursor = self.c.execute(execute)
-        allmsg=[]
+        allmsg = []
         for row in cursor:
-            msgdata= row[0]
-            if(not msgdata):
+            msgdata = row[0]
+            if (not msgdata):
                 continue
-            uin=row[1]
-            ltime=time.localtime(row[2])
-            
-            sendtime=time.strftime("%Y-%m-%d %H:%M:%S",ltime)
-            msg=self.fix(msgdata,0)
-            senderuin=self.fix(uin, 1)
-            
-            amsg=[]
+            uin = row[1]
+            ltime = time.localtime(row[2])
+
+            sendtime = time.strftime("%Y-%m-%d %H:%M:%S", ltime)
+            msg = self.fix(msgdata, 0)
+            senderuin = self.fix(uin, 1)
+
+            amsg = []
             amsg.append(sendtime)
             amsg.append(senderuin)
             amsg.append(msg)
             allmsg.append(amsg)
-        return allmsg    
-    def output(self,num,mode,n1,n2):
+        return allmsg
+
+    def output(self, num, mode, n1, n2):
         first = str(num)[0]
         name1 = n1 if n1 != "" else "我"
         name2 = n2 if n2 != "" else str(num)
-        file=str(num)+".html"
-        f2 = open(file,"w",encoding="utf-8")
-        f2.write("<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /></head>")
-        allmsg=self.message(num,mode)
+        file = str(num) + ".html"
+        f2 = open(file, "w", encoding="utf-8")
+        f2.write(
+            "<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /></head>"
+        )
+        allmsg = self.message(num, mode)
         f2.write("<div style='white-space: pre-line'>")
         for msg in allmsg:
             try:
-                if(msg[1][0]==first):
+                if (msg[1][0] == first):
                     f2.write("<p align='left'>")
                     f2.write("<font color=\"blue\"><b>")
                     f2.write(name2)
@@ -128,22 +141,23 @@ class QQoutput():
                 else:
                     f2.write("<p align='right'>")
                     f2.write("<font color=\"green\">")
-                    f2.write(msg[0])                    
+                    f2.write(msg[0])
                     f2.write("</font>-----<font color=\"blue\"><b>")
                     f2.write(name1)
                     f2.write("</font></b></br>")
                 f2.write(self.AddEmoji(msg[2]))
                 f2.write("</br></br>")
-                f2.write("</p>")               
+                f2.write("</p>")
             except:
                 pass
         f2.write("</div>")
         return self.key
 
+
 def main(db, qq, key, msg, n1, n2):
     try:
         mode = 1
-        q=QQoutput(db,key,mode,msg)
+        q = QQoutput(db, key, mode, msg)
         return q.output(qq, mode, n1, n2)
     except Exception as e:
         with open('log.txt', 'w') as f:
@@ -153,8 +167,10 @@ def main(db, qq, key, msg, n1, n2):
         err_info = repr(e).split(":")[0] == "OperationalError('no such table"
         print(err_info)
         print(repr(e).split(":")[0])
-        if(err_info):
+        if (err_info):
             raise ValueError("QQ号/db地址错误")
+
+
 '''
 db = "C:/Users/30857/Desktop/QQ_History/slowtable_2289785930.db"
 qq = "1962662168"
